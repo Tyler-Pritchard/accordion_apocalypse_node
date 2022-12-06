24ff9fb7160db8d743eab75346232566e65bae4e
Tyler Pritchard <pritchard.tyler@gmail.com>
2022-12-05 17:15:23 -0800

    add http reqs

--- express/server.js
+++ express/server.js
@@ -6,6 +6,47 @@ const port = 3000;
 
 const app = express();
 app.use(morgan('dev'));
+app.use(express.json());
+
+app.all('/accordions', (req, res, next) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    next();
+});
+
+app.get('/accordions', (req, res) => {
+    res.end('Will send all the accordions to you');
+});
+
+app.post('/accordions', (req, res) => {
+    res.end(`Will add the accordion: ${req.body.name} with description: ${req.body.description}`);
+});
+
+app.put('/accordions', (req, res) => {
+    res.end('PUT operation not supported on /accordions');
+});
+
+app.delete('/accordions', (req, res) => {
+    res.end('Deleting all accordions');
+});
+
+app.get('/accordions/:accordionId', (req, res) => {
+    res.end(`Will send details of the accordion: ${req.params.accordionId} to you`);
+});
+
+app.post('/accordions:accordionId', (req, res) => {
+    res.statusCode = 403;
+    res.end(`POST operation not supported on /accordions/${req.params.accordionId}`);
+});
+
+app.put('/accordions:accordionId', (req, res) => {
+    res.write(`Updating the accordion: ${req.params.accordionId}\n`);
+    res.end(`Will update the accordion: ${req.body.name} with description: ${req.body.description}`);
+});
+
+app.delete('/accordions/accordionId', (req, res) => {
+    res.end(`Deleting accordion: ${req.params.accordionId}`);
+})
 
 app.use(express.static(__dirname + '/public'));
 
